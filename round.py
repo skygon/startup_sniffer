fb7b29cccbc91e92fee6d29efc68804318c0d51a
yui <ycui@microstrategy.com>
2018-02-28 13:58:53 +0800

    update

---
+++ round.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+import json
+
+round = {}
+round['1'] = 'Seed'
+round['2'] = 'Angel'
+round['3'] = 'Pre-A'
+round['4'] = 'A'
+round['5'] = 'A+'
+round['6'] = 'Pre-B'
+round['7'] = 'B'
+round['8'] = 'B+'
+round['9'] = 'C'
+round['10'] = 'C'
+round['11'] = 'C+'
+round['12'] = 'D'
+round['13'] = 'D'
+round['14'] = 'D'
+round['15'] = 'E'
+round['16'] = 'F'
+round['17'] = 'G'
+
+print json.dumps(round)
---
+++ round_plus.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import json
+
+round_plus = {}
+round_plus['19'] = 'Pre-IPO'
+round_plus['20'] = u'新三板'
+round_plus['21'] = u'新三板定增'
+round_plus['40'] = 'IPO'
+round_plus['41'] = u'上市'
+round_plus['50'] = u'并购'
+round_plus['60'] = u'战略投资'
+
+print json.dumps(round)
